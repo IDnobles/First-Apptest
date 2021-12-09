1b47373cc8d04f1635233485fea921d48bc323ad
IDnobles <awesomegod019@gmail.com>
2021-12-09 12:01:05 -0500

    My first test App

---
+++ android/app/src/main/java/com/example2/flutter_test1/MainActivity.java
@@ -0,0 +1,8 @@
+package com.example2.fluttertest1;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+//import io.flutter.plugins.firebase.core.FirebaseCorePlugin;
+
+public class MainActivity extends FlutterActivity {
+}
