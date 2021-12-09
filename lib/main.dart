0d6e6aac2667371e4fb9abc86ed1678f34df73f9
IDnobles <awesomegod019@gmail.com>
2021-12-09 13:12:08 -0500

    My first test App

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.yellow,
+        primarySwatch: Colors.yellow, ///sshhhhhhh
 
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
